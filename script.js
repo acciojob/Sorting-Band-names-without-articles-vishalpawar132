b27c419941c20c39cfddb4a48fce15cb2efc0b1f
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-16 09:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-//your code here
 
+document.addEventListener('DOMContentLoaded', function() {
+  let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'Aerosmith'];
+
+  // Sort the band names in lexicographic order excluding articles
+  bandNames.sort(function(a, b) {
+    return removeArticles(a) > removeArticles(b) ? 1 : -1;
+  });
+
+  // Get the <ul> element with id "bands"
+  let ulElement = document.querySelector('#bands');
+
+  // Create and append <li> elements for each band name
+  bandNames.forEach(function(bandName) {
+    let liElement = document.createElement('li');
+    let bandText = document.createTextNode(bandName);
+    liElement.appendChild(bandText);
+    ulElement.appendChild(liElement);
+  });
+
+  // Function to remove articles from the band name
+  function removeArticles(bandName) {
+    return bandName.replace(/^(a|an|the)\s+/i, '');
+  }
+});
